026068fc059bded60d4cec61bcb9436445625177
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-08-11 05:18:23 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
         const commands = new Discord.MessageEmbed()
         commands.setTitle('Commands:')
         commands.addField('-help', 'Gives you a list of commands.')
-        commands.addField('-apply', 'Sends you the link to become an Official Clan on Pistol 1v1.')
+        commands.addField('-apply', 'Sends you the link to become an Official Clan.')
         commands.addField('-credits', 'Shows the bot credits.')
         commands.setColor('#63ace5')
         message.channel.send(commands)
