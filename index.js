626de92af71e5727b4969b2319fd297c00bc7d5d
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-08-21 20:39:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const fs = require('fs');
 
 client.commands = new Discord.Collection();
 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+    client.user.setActivity("iGottic's Youtube Channel.  -help for commands.", { type: "WATCHING"})
+});
+
 client.on('message', message => {
     for (var i = 0; i < badwords.length; i++) {
     if (message.content.includes === (badwords[i])) {
@@ -24,18 +36,6 @@ client.on('message', message => {
     }
     }
 })
-
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-
-    client.commands.set(command.name, command);
-}
-
-client.once('ready', () => {
-    console.log('Bot is online!');
-    client.user.setActivity("iGottic's Youtube Channel.  -help for commands.", { type: "WATCHING"})
-});
 
 client.on('message', message =>{
     if(!message.content.startsWith(prefix) || message.author.bot) return;
