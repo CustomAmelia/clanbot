f7852817b63241b454f5578d05ed711b86b89329
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-15 17:31:07 +0100

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'slowmode',
     description: "lolok",
     execute(message, client, args){
-        if (message.author.id === '498097065264676864' || message.author.id === '266330174948114434' || message.author.id === '400725779006357505' || message.author.id === '400725779006357505') {
+        if (message.author.id === '498097065264676864' || message.author.id === '266330174948114434' || message.author.id === '400725779006357505' || message.author.id === '492816088724144138') {
 
             if (!args) return;
             if (!args[0]) return;
