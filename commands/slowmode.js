865fa87a41a3f92470d65a07e892c0b7708c0438
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-15 18:06:01 +0100

    Create slowmode.js

---
+++ commands/slowmode.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'slowmode',
+    description: "lolok",
+    execute(message, client, args){
+        if (message.author.id === '498097065264676864' || message.author.id === '266330174948114434' || message.author.id === '400725779006357505' || message.author.id === '492816088724144138') {
+
+            if (!args) return;
+            if (!args[0]) return;
+            if (args[0].length > 3) {
+                message.channel.send('You cannot make a channels slowmode more than 5 minutes.')
+            }
+
+            if (args[0].length === 3) {
+                message.channel.setRateLimitPerUser(args[0])
+            }
+
+            if (args[0].length > 3) {
+            message.channel.setRateLimitPerUser(args[0])
+            }
+        }
+    }
+}
