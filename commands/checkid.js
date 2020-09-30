4ce80f1e600b13971d56814676bf6cd3203c5cea
BeamV1 <66842247+BeamV1@users.noreply.github.com>
2020-09-30 16:53:23 +0100

    Update checkid.js

--- commands/checkid.js
+++ commands/checkid.js
@@ -5,10 +5,6 @@ module.exports = {
     name: 'check',
     description: "test",
     execute(message, client, args){
-        if (!args[0]) {
-            message.channel.send('Please provide a user id.')
-        }
-
         const api = `https://verify.eryn.io/api/roblox/${args[0]}`
         snekfetch.get(api).then(console.log(Response))
     }
